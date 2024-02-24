9e0d4fdaaa36b5a95081d0e2056fbceff91a5424
rahmatilla <rahmatilla301993@gmail.com>
2024-02-24 15:48:43 +0500

    spring-mvc intro

---
+++ src/main/java/org/example/domain/Todo.java
@@ -0,0 +1,19 @@
+package org.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Todo {
+    private int id;
+    private String title;
+    private String priority;
+    private LocalDateTime createdAt;
+}
