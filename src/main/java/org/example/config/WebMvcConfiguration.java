e1ba5f1cec9172be8557f1930650ff829b6632fc
rahmatilla <rahmatilla301993@gmail.com>
2024-02-25 13:39:34 +0500

    spring-mvc with thymeleaf intro

--- src/main/java/org/example/config/WebMvcConfiguration.java
+++ src/main/java/org/example/config/WebMvcConfiguration.java
@@ -1,12 +1,60 @@
 package org.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("org.example")
 public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public WebMvcConfiguration(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/*")
+                .addResourceLocations("classpath:/static/css/");
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
 }
--- src/main/java/org/example/controller/TodoController.java
+++ src/main/java/org/example/controller/TodoController.java
@@ -1,45 +1,17 @@
 package org.example.controller;
 
-import org.example.domain.Todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@ResponseBody
 public class TodoController {
 
-    private final List<Todo> todos = List.of(
-            new Todo(1, "Have a lunch", "high", LocalDateTime.now()),
-            new Todo(2, "Read the book", "low", LocalDateTime.now()),
-            new Todo(3, "Go to a bed", "medium", LocalDateTime.now()),
-            new Todo(4, "Playing football", "high", LocalDateTime.now())
-    );
-
-    @GetMapping("/todos")
-    public String getAll() {
-        StringBuilder sb = new StringBuilder();
-        todos.forEach(todo -> sb.append(
-                """
-                                <tr>
-                                    <td>%s</td>
-                                    <td>%s</td>
-                                    <td>%s</td>
-                                  </tr>
-                        """.formatted(todo.getId(), todo.getTitle(), todo.getPriority())
-        ));
-        return """
-                <table border=1>
-                  <tr>
-                    <th>Id</th>
-                    <th>Title</th>
-                    <th>Priority</th>
-                  </tr>
-                %s
-                </table>
-                """.formatted(sb.toString());
+    @GetMapping("/")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("username", "John");
+        return modelAndView;
     }
 }
